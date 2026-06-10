5f77d4f508b63b2689980929bd04e441e1c01b09
P34 <amirpea4444@gmail.com>
2026-06-10 07:29:01 +0100

    Change fatal log to warning for scheduling issue
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- system/btif/src/btif_a2dp_source.cc
+++ system/btif/src/btif_a2dp_source.cc
@@ -335,7 +335,7 @@ bool btif_a2dp_source_init(void) {
   if (com_android_bluetooth_flags_a2dp_source_null_fixed_queue()) {
     if (!btif_a2dp_source_thread.EnableRealTimeScheduling()) {
 #if defined(__ANDROID__)
-      log::fatal("unable to enable real time scheduling");
+      log::warn("unable to enable real time scheduling");
 #endif
     }
   }
